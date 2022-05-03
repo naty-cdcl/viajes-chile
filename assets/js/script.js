8939c0840e6d4331c77bed504db678951e2b1433
naty-cdcl <nacd1988@hotmail.com>
2022-05-03 04:48:50 -0400

    se añadio mensaje de alerta en enviar-contacto, y se añadieron tooltips en los iconos de rrss

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+//alerta mensaje enviado
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('enviar')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Mensaje enviado con exito.', 'success')
+  })
+}
