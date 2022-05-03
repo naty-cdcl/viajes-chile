2e32c0c0ae12b097b270bfcc508322a4c6bef909
naty-cdcl <nacd1988@hotmail.com>
2022-05-03 04:21:09 -0400

    documentacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//tooltip script
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
